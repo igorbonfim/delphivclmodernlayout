6c12652a81745b6b6537a440e431326e464a7f56
Igor Bonfim <igorbonfim88@gmail.com>
2024-07-15 23:39:14 -0300

    Criação da tabela Menu

--- Queries_projeto.sql
+++ Queries_projeto.sql
@@ -7,9 +7,18 @@ CREATE TABLE Banco (
 	PRIMARY KEY (BancoId)
 );
 
+CREATE TABLE Menu (
+	menuId int identity(1,1) NOT NULL,
+	titulo varchar(25) NOT NULL,
+	nomeImagem varchar(100) NOT NULL,
+	nomeFormulario varchar(50) NOT NULL,
+	processo CHAR(3) NOT NULL
+
+	primary key(menuId)
+);
+
 INSERT INTO Banco (CodBanco, DescBanco, WebSite) VALUES('001', 'Banco do Brasil S.A', 'www.bb.com.br');
 INSERT INTO Banco (CodBanco, DescBanco, WebSite) VALUES('003', 'Banco da Amazônia S.A', 'www.bancoamazonia.com.br');
 INSERT INTO Banco (CodBanco, DescBanco, WebSite) VALUES('004', 'Banco do Nordeste do Brasil S.A', 'www.banconordeste.gov.br');
 INSERT INTO Banco (CodBanco, DescBanco, WebSite) VALUES('104', 'Caixa Econômica Federal', 'www.caixa.gov.br');
 
-select * from banco;
