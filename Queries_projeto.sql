293146a155b4635860d834c33c2f9da664f7f174
Igor Bonfim <igorbonfim88@gmail.com>
2024-08-24 10:26:37 -0300

    Criação da tabela de cartão e atualização das queries do projeto

--- Queries_projeto.sql
+++ Queries_projeto.sql
@@ -1,3 +1,4 @@
+-- Criação das tabelas
 CREATE TABLE Banco (
 	BancoId int identity(1,1) NOT NULL,
 	CodBanco varchar(03) unique NOT NULL,
@@ -17,14 +18,19 @@ CREATE TABLE Menu (
 	primary key(menuId)
 );
 
+CREATE TABLE Cartao (
+	cartaoId int identity NOT NULL,
+	descricaoCartao varchar(30) NOT NULL,
+	primary key(cartaoId)
+)
+
+-- Bancos
 INSERT INTO Banco (CodBanco, DescBanco, WebSite) VALUES('001', 'Banco do Brasil S.A', 'www.bb.com.br');
 INSERT INTO Banco (CodBanco, DescBanco, WebSite) VALUES('003', 'Banco da Amazônia S.A', 'www.bancoamazonia.com.br');
 INSERT INTO Banco (CodBanco, DescBanco, WebSite) VALUES('004', 'Banco do Nordeste do Brasil S.A', 'www.banconordeste.gov.br');
 INSERT INTO Banco (CodBanco, DescBanco, WebSite) VALUES('104', 'Caixa Econômica Federal', 'www.caixa.gov.br');
-
-insert into Menu(titulo, nomeImagem, nomeFormulario, processo) values('Cadastro de Banco', 'BancoFebraban.png', 'TFrmBancoListagem', 'FIN');
-
-insert into Menu(titulo, nomeImagem, nomeFormulario, processo) values('Cadastro de Cartão', 'CartaoCredito.png', 'TFrmBancoListagem', 'FIN');
-
-insert into Menu(titulo, nomeImagem, nomeFormulario, processo) values('Configurações', 'Configuracao.png', 'TFrmBancoListagem', 'CNF');
+-- Opções do menu
+INSERT INTO Menu(titulo, nomeImagem, nomeFormulario, processo) values('Cadastro de Banco', 'BancoFebraban.png', 'TFrmBancoListagem', 'FIN');
+INSERT INTO Menu(titulo, nomeImagem, nomeFormulario, processo) values('Cadastro de Cartão', 'CartaoCredito.png', 'TFrmBancoListagem', 'FIN');
+INSERT INTO Menu(titulo, nomeImagem, nomeFormulario, processo) values('Configurações', 'Configuracao.png', 'TFrmBancoListagem', 'CNF');
 
