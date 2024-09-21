a2e92ea24de7691dbcc266f775e128e9c13d4c62
Igor Bonfim <igorbonfim88@gmail.com>
2024-09-21 00:35:14 -0300

    Reformulação da tela de login

--- uLogin.pas
+++ uLogin.pas
@@ -6,17 +6,27 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.ExtCtrls,
   Vcl.StdCtrls, JvExStdCtrls, JvButton, JvCtrls, System.ImageList, Vcl.ImgList,
-  Data.DB, ZAbstractRODataset, ZAbstractDataset, ZDataset, uFuncoes;
+  Data.DB, ZAbstractRODataset, ZAbstractDataset, ZDataset, uFuncoes, uDtmConexao;
 
 type
   TfrmLogin = class(TForm)
-    Image1: TImage;
     btnCancelar: TJvImgBtn;
     btnAcessar: TJvImgBtn;
     imgButtons: TImageList;
     edtUsuario: TEdit;
     edtSenha: TEdit;
     QryLogin: TZQuery;
+    QryLoginusuarioId: TIntegerField;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    Label1: TLabel;
+    Image1: TImage;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Image2: TImage;
     procedure btnCancelarClick(Sender: TObject);
     procedure btnAcessarClick(Sender: TObject);
     procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
